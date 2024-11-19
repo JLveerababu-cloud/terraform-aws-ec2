e90482767193992b01846ba5efacfbbc8b97ca67
veerababu <jlveerababu99@gmail.com>
2024-11-19 16:42:14 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "terraform" {
-    #count = terraform.workspace == "prod" ? 2 : 1
-    ami = "ami-09c813fb71547fc4f"
-    instance_type ="t3.micro"
-    vpc_security_group_ids = ["sg-0238e87f7726f8306"]
-   
+    count = var
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group_ids
 }
